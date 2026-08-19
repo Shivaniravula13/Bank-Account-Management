9fb0d0ec79ecb3b391cc915cdc090616ee3f0d36
ShatagopamSrivalli <srivallishatagopam@gmail.com>
2026-08-19 23:21:28 +0530

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -11,7 +11,7 @@ public class MainApp {
 
         // Create Customer
         Customer customer = new Customer(
-                "Srivalli",
+                "Tharunya",
                 21,
                 "C001",
                 address
